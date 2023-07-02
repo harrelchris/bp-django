14d87ad3d26da451693492c9dbf9d78a1f5565ca
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-07-02 11:49:16 -0500

    add logging example

--- app/public/views.py
+++ app/public/views.py
@@ -1,5 +1,14 @@
+import logging
+
 from django.views.generic import TemplateView
+
+logger = logging.getLogger("app")
 
 
 class IndexView(TemplateView):
     template_name = "public/index.html"
+
+    def get(self, request, *args, **kwargs):
+        logger.debug("Debug log")
+        logger.error("Error log")
+        return super().get(request, *args, **kwargs)
