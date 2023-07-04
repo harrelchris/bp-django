0ec945144f966a9d90c4eb80f2e2f90248f9af5b
harrelchris <24297899+harrelchris@users.noreply.github.com>
2023-07-03 23:17:11 -0500

    automate setup

---
+++ scripts/setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+VENV=".venvv"
+
+# Create virtual environment
+if [ ! -d "$VENV" ]; then
+    python3 -m venv $VENV
+fi
+
+# Activate virtual environment
+source $VENV/bin/activate
+
+# Update virtual environment
+python3 -m pip install pip setuptools wheel --upgrade
+
+# Install dependencies
+pip install -r requirements-dev.txt --upgrade
+
+# Create .env from example
+if [ ! -d ".env" ]; then
+    cp .env.example .env
+fi
+
+# Delete the existing database
+if [ ! -d "db.sqlite3" ]; then
+    rm "db.sqlite3"
+fi
+
+#:: Initialize a clean database
+python3 app/manage.py makemigrations
+python3 app/manage.py migrate
+
+# Delete migrations during development
+# rm app\public\migrations\0001_initial.py
+
+# Create super user
+python3 app/manage.py createsuperuser --noinput
