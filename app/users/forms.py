5d5bc2773c21ef5d64afe03aa2db2e13bf5a2f46
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-02-16 13:27:42 -0600

    Specify model and fields for registration form

--- app/users/forms.py
+++ app/users/forms.py
@@ -59,6 +59,15 @@ class RegisterForm(UserCreationForm):
         "password2",
     ]
 
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
