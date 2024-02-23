53937ca40c38ace6e024672b0d1d62f1a62d4db5
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-02-22 20:40:36 -0600

    Divide install script

--- deploy/install.sh
+++ deploy/install.sh
@@ -4,46 +4,17 @@
 apt update
 apt upgrade -y
 
-# Install dependencies
-apt install nginx postgresql python3-venv ufw -y
-
 # Configure UFW
-ufw --force disable
-ufw --force reset
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow http
-ufw allow https
-sed -i "s/IPV6=no/IPV6=yes/" /etc/default/ufw
-ufw --force disable
-ufw --force enable
-systemctl restart ufw
+./srv/web/deploy/ufw.sh
 
 # Create database
-sudo -u postgres psql -c "CREATE USER web WITH ENCRYPTED PASSWORD 'pass';"
-sudo -u postgres psql -c "CREATE DATABASE web;"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE web TO web;"
+./srv/web/deploy/postgres.sh
 
 # Install application
-python3 -m venv /srv/web/venv
-/srv/web/venv/bin/python3 -m pip install pip setuptools wheel --upgrade --no-cache-dir
-/srv/web/venv/bin/python3 -m pip install -r /srv/web/requirements/prod.txt --upgrade --no-cache-dir
-cp /srv/web/envs/prod.env /srv/web/.env
-SECRET_KEY=$(python3 -c "import secrets;print(secrets.token_urlsafe(64))")
-sed -i "s/<SECRET_KEY>/$SECRET_KEY/g" /srv/web/.env
-/srv/web/venv/bin/python3 /srv/web/app/manage.py collectstatic
-/srv/web/venv/bin/python3 /srv/web/app/manage.py migrate
+./srv/web/deploy/app.sh
 
 # Create gunicorn socket and service
-cp /srv/web/deploy/gunicorn.service /etc/systemd/system/gunicorn.service
-cp /srv/web/deploy/gunicorn.socket /etc/systemd/system/gunicorn.socket
-systemctl daemon-reload
-systemctl start gunicorn.socket
-systemctl enable gunicorn.socket
+./srv/web/deploy/gunicorn.sh
 
 # Configure NGINX
-cp /srv/web/deploy/app.conf /etc/nginx/conf.d/app.conf
-PUBLIC_IP=$(curl -s -4 ifconfig.me)
-sed -i "s/server_name ~^.+$;/server_name $PUBLIC_IP;/" /etc/nginx/conf.d/app.conf
-systemctl restart nginx
+./srv/web/deploy/nginx.sh
