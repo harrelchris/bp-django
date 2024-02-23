79e3d74b5de60a06880f6b5ddbd57f8b9c81d1a5
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-02-22 21:06:57 -0600

    Execute with source

--- deploy/install.sh
+++ deploy/install.sh
@@ -5,16 +5,16 @@ apt update
 apt upgrade -y
 
 # Configure UFW
-./srv/web/deploy/ufw.sh
+source /srv/web/deploy/ufw.sh
 
 # Create database
-./srv/web/deploy/postgres.sh
+source /srv/web/deploy/postgres.sh
 
 # Install application
-./srv/web/deploy/app.sh
+source /srv/web/deploy/app.sh
 
 # Create gunicorn socket and service
-./srv/web/deploy/gunicorn.sh
+source /srv/web/deploy/gunicorn.sh
 
 # Configure NGINX
-./srv/web/deploy/nginx.sh
+source /srv/web/deploy/nginx.sh
