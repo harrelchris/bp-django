e44bedb11de0a53e9a2a0e4f4e86dd6c07a1a7c6
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-02-24 07:24:37 -0600

    User catchall server_name

--- deploy/nginx.sh
+++ deploy/nginx.sh
@@ -3,6 +3,11 @@
 apt install nginx -y
 
 cp /srv/web/deploy/app.conf /etc/nginx/conf.d/app.conf
-PUBLIC_IP=$(curl -s -4 ifconfig.me)
-sed -i "s/server_name ~^.+$;/server_name $PUBLIC_IP;/" /etc/nginx/conf.d/app.conf
+
+# Set the server name to the public IP address
+# PUBLIC_IP=$(curl -s -4 ifconfig.me)
+# sed -i "s/server_name ~^.+$;/server_name $PUBLIC_IP;/" /etc/nginx/conf.d/app.conf
+
 systemctl restart nginx
+systemctl enable nginx
+systemctl daemon-reload
